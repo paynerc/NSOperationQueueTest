5a8a76b831456c5f8f9ca0c428ce680462b4f297
Ryan C. Payne <rpayne-oss@bullittsystems.com>
2013-02-12 16:06:39 -0500

    Initial Commit

---
+++ NSOperationQueueTest/JJZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JJZAppDelegate.h
+//  NSOperationQueueTest
+//
+//  Created by Ryan C. Payne on 2/12/13.
+//  Copyright (c) 2013 BullittSystems, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface JJZAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ NSOperationQueueTest/JJZAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  JJZAppDelegate.m
+//  NSOperationQueueTest
+//
+//  Created by Ryan C. Payne on 2/12/13.
+//  Copyright (c) 2013 BullittSystems, Inc. All rights reserved.
+//
+
+#import "JJZAppDelegate.h"
+
+@implementation JJZAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
